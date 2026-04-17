432e917dea624365441d16886e57ca5ed1c118ee
isabellachoukaira <i.choukaira@gmail.com>
2026-04-16 22:58:21 -0300

    test: adiciona script de validacao para a gramatica main

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -28,3 +28,17 @@ def test_return_identifier():
     result = parser.parse_return()
 
     assert result.value == "y"
+
+def test_parse_block(self):
+        code = "{ int x = 5; return x; }"
+        parser = Parser(Lexer(code))
+        result = parser.parse_block()
+        # Valida se o bloco foi criado e tem 2 comandos
+        self.assertEqual(len(result.statements), 2)
+
+def test_parse_main_program(self):
+        code = "int main() { return 0; }"
+        parser = Parser(Lexer(code))
+        result = parser.parse_program()
+        # Valida se a Main foi reconhecida e tem um corpo (Block)
+        self.assertIsNotNone(result.body)
