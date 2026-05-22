30de49a64c80aac9fa650e5f41ef2961e1c40b4c
Beatriz Lins <beageane@gmail.com>
2026-05-22 16:11:45 -0300

    test: adiciona caso de teste para estruturas de repetição e lógica

--- tests/teste.c
+++ tests/teste.c
@@ -1,20 +1,8 @@
-int calcular_desconto(int valor) {
-    int resultado = 0;
-    
-    if (valor > 100) {
-        resultado = valor - 20;
-    } else {
-        resultado = valor - 5;
-    }
-    
-    return resultado;
-}
-
 int main() {
-    int preco = 150;
-    int preco_final = 0;
-    
-    preco_final = calcular_desconto(preco);
-    
+    for (int i = 0; i < 5; i++) {
+        if (i == 2) {
+            continue;
+        }
+    }
     return 0;
 }
