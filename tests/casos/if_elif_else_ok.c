d4b575c1f666653af4c2f2d2e405fa803a0e130b
dudaa28 <eduardaq2805@gmail.com>
2026-05-30 17:59:27 -0300

    Adiciona casos de teste para mapeamento if/elif/else

---
+++ tests/casos/if_elif_else_ok.c
@@ -0,0 +1,11 @@
+int main() {
+    int x = 10;
+    if (x > 10) {
+        int y = 1;
+    } else if (x == 10) {
+        int y = 2;
+    } else {
+        int y = 3;
+    }
+    return 0;
+}
---
+++ tests/casos/if_else_ok.c
@@ -0,0 +1,9 @@
+int main() {
+    int x = 5;
+    if (x > 10) {
+        int y = 1;
+    } else {
+        int y = 2;
+    }
+    return 0;
+}
